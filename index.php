4be0fec46b3b40bdd53897c2be29c62d216f31f9
Joao <joaogoya@gmail.com>
2022-12-06 15:11:56 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * Plugin Name: Pipeline imagens para SEO
+ * Plugin URI: http://pipeline-digital.com.br/
+ * Description: Plugin para tratamento de imagens para SEO.
+ * Author: Pipeline Digital
+ * Version: 1.0.0 
+ * Author URI: http://pipeline-digital.com.br/
+ * License: GPL2+ 
+*/
+
+define('ARQ_PRINCIPAL', __FILE__);
+
+require_once('img-atributes.php');
+
+
+
+
